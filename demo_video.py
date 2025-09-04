07399902faeaabdc134dc9ef2e735436b801c357
Ahmad Al-Salem <ata83@miami.edu>
2025-09-04 14:08:27 -0400

    video can work on hand now

--- demo_video.py
+++ demo_video.py
@@ -49,14 +49,17 @@ def process_frame(frame, body=True, hands=True):
         candidate, subset = body_estimation(frame)
         canvas = util.draw_bodypose(canvas, candidate, subset)
     if hands:
-        hands_list = util.handDetect(candidate, subset, frame)
-        all_hand_peaks = []
-        for x, y, w, is_left in hands_list:
-            peaks = hand_estimation(frame[y:y+w, x:x+w, :])
-            peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
-            peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
-            all_hand_peaks.append(peaks)
-        canvas = util.draw_handpose(canvas, all_hand_peaks)
+        if body:
+            hands_list = util.handDetect(candidate, subset, frame)
+            for x, y, w, is_left in hands_list:
+                peaks = hand_estimation(frame[y:y+w, x:x+w, :])
+                peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
+                peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
+                canvas = util.draw_handpose_by_opencv(canvas, peaks)
+        else:
+            # If no body, run hand model on the whole frame
+            peaks = hand_estimation(frame)
+            canvas = util.draw_handpose_by_opencv(canvas, peaks)
     return canvas
 
 # writing video with ffmpeg because cv2 writer failed
